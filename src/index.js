373eeae443d5e1ff86ee8ca047722f90fca0e700
Ion Gireada <ioncakephper@users.noreply.github.com>
2025-08-25 19:09:56 +0300

    Fix: CLI help message test and behavior

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function buildProgram() {
+  const { Command } = require("commander");
+  const program = new Command();
+  const pkg = require("../package.json");
+  const { version } = pkg;
+
+  program
+    .name("prodpage")
+    .description("A CLI tool for demonstration purposes")
+    .version(version, "-v, --version", "output the current version")
+    .option("-d, --debug", "enable debug mode")
+    .option("-c, --config <path>", "set config path", "./config.json");
+
+  program
+    .command("start")
+    .description("Start the application")
+    .action(() => {
+      console.log("Application started");
+      if (program.opts().debug) {
+        console.log("Debug mode is enabled");
+      }
+      console.log(`Using config file: ${program.opts().config}`);
+    });
+
+  program
+    .command("stop")
+    .description("Stop the application")
+    .action(() => {
+      console.log("Application stopped");
+    });
+
+  return program;
+}
+
+    function run() {
+    const program = buildProgram();
+    program.parse(process.argv);
+
+    // If no arguments are provided, display help
+    if (process.argv.length < 3) {
+      program.help();
+    }
+  }
+
+  run();
+
+module.exports = { buildProgram, run };
